8000cbbca1c7abb02eec6e6aac49e41342580583
ciaochiaociao <tony790927@gmail.com>
2020-02-21 13:14:04 +0800

    (refactor) separate modules and functions

---
+++ config.py
@@ -0,0 +1,10 @@
+#  Copyright (c) 2020. The Natural Language Understanding Lab, Institute of Information Science, Academia Sinica - All Rights Reserved
+#  Unauthorized copying of this file, via any medium is strictly prohibited
+#  Proprietary and confidential
+#  Written by Chiao-Wei Hsu <cwhsu@iis.sinica.edu.tw>
+
+from opencc import OpenCC
+
+cc = OpenCC('t2s')
+DEFAULT_CORENLP_IP = 'http://140.109.19.191:9000'
+FGC_KB_PATH = 'fgc_knowledgebase.json'
