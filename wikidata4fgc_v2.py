ea2b0cbdf3ccec3d6e301bf4b3080bd074c40420
ciaochiaociao <tony790927@gmail.com>
2020-02-25 00:06:00 +0800

    Update Copyright

--- wikidata4fgc_v2.py
+++ wikidata4fgc_v2.py
@@ -1,3 +1,8 @@
+#   Copyright (c) 2020. The NLU Lab, Institute of Information Science, Academia Sinica - All Rights Reserved
+#   Unauthorized copying of this file, via any medium is strictly prohibited
+#   Proprietary and confidential
+#   Written by Chiao-Wei Hsu <cwhsu@iis.sinica.edu.tw>
+
 from pymongo import MongoClient
 from typing import Dict, List
 from opencc import OpenCC
