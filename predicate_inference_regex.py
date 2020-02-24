9707a1f1ef398300ae3ccd2896c95d35ada1de2c
ciaochiaociao <tony790927@gmail.com>
2020-02-24 23:49:02 +0800

    (bugfix) 出生"於", "的", "是" (todo: check again in other attributes)

--- predicate_inference_regex.py
+++ predicate_inference_regex.py
@@ -1,12 +1,12 @@
-#  Copyright (c) 2020. The Natural Language Understanding Lab, Institute of Information Science, Academia Sinica - All Rights Reserved
-#  Unauthorized copying of this file, via any medium is strictly prohibited
-#  Proprietary and confidential
-#  Written by Chiao-Wei Hsu <cwhsu@iis.sinica.edu.tw>
+#   Copyright (c) 2020. The NLU Lab, Institute of Information Science, Academia Sinica - All Rights Reserved
+#   Unauthorized copying of this file, via any medium is strictly prohibited
+#   Proprietary and confidential
+#   Written by Chiao-Wei Hsu <cwhsu@iis.sinica.edu.tw>
 
 # rule 2-1-0: verbs/nouns/others
 
 birth = '(出生|诞生)'
-birth_all = f'({birth}|生)'
+birth_all = f'(生|{birth})'
 death = f'(死亡|死掉|过世|逝世|去世|离世|离开人间)'
 death_sim = f'死'
 death_all = f'({death}|{death_sim})'
@@ -61,22 +61,22 @@ is_ = '(叫|是)'
 height = '((总)高度|身高)'
 have = '(有|拥有)'
 strict_label_map = {
-    '名字': [f'^{name}{is_}{what}名字', f'^{name}{is_}名字{what}', f'^{name}(的)?名字{is_}{what}'],
+    '名字': [f'^{name}{is_}{what}名字', f'^{name}(的)?名字{what}', f'^{name}(的)?名字{is_}{what}'],
     # rule 2-1-a: 地方
     '出生地': [f'^{name}的老家', f'^{name}(的)?{birth}.*{place_all}',
-            f'^{name}{birth_all}.*{where_all}', f'{where_all}{is_}{name}的{birth_all}',
+            f'^{name}{birth_all}.*{where_all}', f'{where_all}{is_}{name}(的)?{birth_all}',
             f'^{name}{is_}{where_all}(的)?人', f'^{name}({is_})?{where_all}来'],
     '死亡地': [f'^{name}的死亡地', f'^{name}{death_all}{_in}{place_all}',
             f'^{name}{_in}{where_all}{death_all}',
             f'^{name}{death_all}{_in}{where_all}', f'{where_all}{is_}{name}的{death_all}'],
     '墓地': [],
     # rule 2-1-b: 时间
-    '出生日期': [f'^{name}的生日', f'^{name}的出生日期', f'^{name}的出生日',
-             f'^{name}{when}.*{birth_all}', f'^{name}的{birth_all}.*{when}'],
-    '死亡日期': [f'^{name}的死亡地',
-             f'^{name}{when}.*{death_all}', f'^{name}的{death_all}.*{when}'],
-    '成立或建立時間': [f'^{name}的{start}.*{time}',
-                f'^{name}{when}.*{start}', f'^{name}{start}.*{when}'],
+    '出生日期': [f'^{name}(的)?生日', f'^{name}(的)?出生日期', f'^{name}(的)?出生日',
+             f'^{name}(是|{_in})?{when}.*{birth_all}', f'^{name}(的)?{birth_all}.*{when}'],
+    '死亡日期': [f'^{name}(的)?死亡地',
+             f'^{name}(是|{_in})?{when}.*{death_all}', f'^{name}(的)?{death_all}.*{when}'],
+    '成立或建立時間': [f'^{name}(的)?{start}.*{time}',
+                f'^{name}(是|{_in})?{when}.*{start}', f'^{name}{start}.*{when}'],
     # rule 2-1-c: 人物资讯
     '國籍': [f'^{name}的国籍', f'^{name}{is_}{what_which}(朝代|国籍)',
            f'^{name}(的)?{birth_all}.*{what_which}({country}|朝代|国籍)', f'^{name}({is_})?{what_which}({country}|朝代|国籍){birth_all}',
