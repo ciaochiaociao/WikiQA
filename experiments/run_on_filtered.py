4f16336af350c81d906da2c363d9e140f8dad918
ciaochiaociao <tony790927@gmail.com>
2020-03-01 02:10:38 +0800

    exp_and_check_errors_on_v0.1

--- experiments/run_on_filtered.py
+++ experiments/run_on_filtered.py
@@ -10,19 +10,18 @@ import pandas
 from fgc_utils import get_docs_with_certain_qs
 from wikiqa import WikiQA
 
-with open('../FGC_release_all(cn).json', encoding='utf-8') as f:
+with open('../FGC_release_all(cn)_filtered.json', encoding='utf-8') as f:
     docs = json.load(f)
 
 wiki_qa = WikiQA(server='http://140.109.19.191:9000')
 
-df = pandas.read_csv('fgc_wiki_benchmark_v0.1.tsv', sep='\t')
+docs = get_docs_with_certain_qs(['D002Q01'], docs)
 
-qids = df.id.to_list()
-filtered_docs = get_docs_with_certain_qs(qids, docs)
-
-with open('model_v0.5_on_bm_v0.1/file4eval.tsv', 'w', encoding='utf-8') as file4eval:
+all_answers = []
+with open('file4eval_filtered.tsv', 'w', encoding='utf-8') as file4eval:
     print('qid\tparsed_subj\tparsed_pred\tsid\tpretty_values\tproc_values\tanswers\tanswer', file=file4eval)
-    for doc in filtered_docs:
+    for doc in docs:
         answers = wiki_qa.predict_on_qs_of_one_doc(doc, use_fgc_kb=False, file4eval=file4eval)
-        
-        # break
+        all_answers.extend(answers)
+        print(answers)
+print(all_answers)
