d4453851b49ef4ab64c45b732cf8d7c4c06641d3
ciaochiaociao <ciaochiaociao@gmail.com>
2020-03-22 22:00:21 +0800

    Add TeeLogger Utils

--- fgc_wiki_qa/utils/utils.py
+++ fgc_wiki_qa/utils/utils.py
@@ -3,9 +3,27 @@
 #   Proprietary and confidential
 #   Written by Chiao-Wei Hsu <cwhsu@iis.sinica.edu.tw>
 import re
+import sys
+import json
+
 from ansi.colour import fg, bg
 
-import json
+
+class TeeLogger(object):
+
+    def __init__(self, fpath, mode='w', **kwargs):
+        self.terminal = sys.stdout
+        self.log = open(fpath, mode, **kwargs)
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+        #you might want to specify some extra behavior here.
+        pass
 
 
 def in_ranges(ix, ranges):
