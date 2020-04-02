b6364a728df744b74a37509a16b3bea31985ee90
ciaochiaociao <ciaochiaociao@gmail.com>
2020-04-02 23:46:41 +0800

    Add new correct IDs

--- fgc_wiki_qa/commands/evaluate.py
+++ fgc_wiki_qa/commands/evaluate.py
@@ -26,10 +26,16 @@ argparser.add_argument('--error_analysis', default='experiments/new/error_analys
 
 args = argparser.parse_args()
 # from rule-based model v0.7
-already_corrects = ['D001Q01', 'D001Q03', 'D001Q06', 'D004Q06', 'D071Q12', 'D072Q04', 'D097Q04', 'D097Q05',
-                    'D247Q09',
-                    'D274Q01', 'D275Q01', 'D284Q01', 'D285Q01', 'D293Q01', 'D305Q01', 'D305Q06', 'D305Q08']
-already_errors = ['D001Q09', 'D071Q01', 'D071Q13', 'D077Q10', 'D254Q01', 'D247Q09', 'D275Q03', 'D302Q01']
+already_corrects = \
+    ['D004Q05', 'D004Q06', 'D071Q01', 'D071Q02', 'D071Q12', 'D071Q13', 'D072Q04', 'D274Q01', 'D275Q01', 'D284Q01', 'D293Q01', 'D305Q01', 'D305Q06', 'D305Q08', 'D001Q01', 'D001Q03', 'D001Q06', 'D097Q04', 'D097Q05', 'D285Q01']
+    # ['D001Q01', 'D001Q03', 'D001Q06', 'D004Q06', 'D071Q12', 'D072Q04', 'D097Q04', 'D097Q05',
+    #                 'D274Q01', 'D275Q01', 'D284Q01', 'D285Q01', 'D293Q01', 'D305Q01', 'D305Q06', 'D305Q08']\
+    # + ['D004Q05'] # v0.7_on_1.5
+
+already_errors = \
+    ['D302Q01', 'D001Q09']
+    # ['D001Q09', 'D071Q01', 'D071Q13', 'D077Q10', 'D254Q01', 'D247Q09', 'D275Q03', 'D302Q01']
+
 # already_corrects = set(already_corrects) & set(qids)
 evaluate(already_corrects, already_errors, args.fgc_fpath, args.eval_fpath, TeeLogger(args.result_fpath))
 error_analysis(args.fgc_qa_fpath, args.wiki_benchmark, args.eval_fpath, args.error_analysis)
